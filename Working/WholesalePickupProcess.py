63c63562421abc79748bd7e7013ea63b17ffb619
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-02-09 17:06:42 +0800

    (2nd update) WholesalePickupProcess
    - Remove the location and password to PathNPassword.py
    - Add to cpature the latest file for working file

--- Working/WholesalePickupProcess.py
+++ Working/WholesalePickupProcess.py
@@ -8,23 +8,22 @@ import win32com.client as win32
 from win32com.client import constants
 excel = win32.gencache.EnsureDispatch('Excel.Application')
 from ctypes import windll
+import PathNPassword
 
-"""
-# TODO: Save the Path and Data in another python file
 
-# Path for all files
-VMRH = 'D:\\Python\\Book 3'
-SCC = 'D:\\Python\\Book 4'
-PARIS = 'D:\\Python\\Book 5'
-OriginalPath = [VMRH, SCC, PARIS]
-"""
-# Path to store the Revenue Data
-Revenue_Data = 'D:\\Python\\Testing'
+# Import Revenue file location from PathNPassword
+VMRH = PathNPassword.VMRH
+SCC = PathNPassword.SCC
+PARIS = PathNPassword.PARIS
+OriginalPath = PathNPassword.OriginalPath
 
-# Woring file path and password
-Working_File = 'D:\\Python\\Additional\\WholesalePickupProcess\\Wholesale Pick-up ComparisonTesting2.xlsx'
-password = 'venetian2019'
-"""
+# Import the path to store Revenue data from PathNPassword
+Revenue_Data = PathNPassword.Revenue_Data
+
+# Import the password and working file path from PathNPassword
+Working_File_Path = PathNPassword.Working_File_Path
+password = PathNPassword.password
+
 # Revenue File path for data to be copied
 LocationOfFile = []
     
@@ -48,18 +47,20 @@ for path2 in OriginalPath:
 
 # Remove last week's excel file in the specific folder
 for file in os.listdir(Revenue_Data):
-    print(file)
     send2trash.send2trash(Revenue_Data + "\\" + file)
 
 # Move four files to specific folder
 for path3 in LocationOfFile:
     shutil.copy(path3, Revenue_Data)
-"""
+
+# Find the latest working file and the Filename
+Working_File = FileLocation(Working_File_Path)
+Working_Filename = os.path.basename(Working_File)
+
 
 # Dictionary to search for column in our working file 
 Month = {'January': '4', 'February': '5', 'March': '6', 'April': '7', 'May': '8', 'June': '9',
          'July': '10', 'August': '11', 'September': '12', 'October': '13', 'November': '14', 'December': '15'}
-
 
 # Find the correct column, then copy data in revenue data and paste it in our working file
 def CopyNPaste(ws, Prop):
@@ -93,9 +94,8 @@ def CopyNPaste(ws, Prop):
 # Open our working file by using password
 wb1 = excel.Workbooks.Open(Working_File, False, False, None, password, password)
 excel.Visible = True
-# TODO: Need to find filename
 # Run Excel Macro in Working file
-#excel.Application.Run(".xlsm!Module1.ClearData")
+#excel.Application.Run(Working_Filename + "!Module1.ClearData")
         
 # Open Revenue Report for copying data to our working file
 # Seperate into two conditions as HIMCC and CMCC are in the same file
@@ -130,3 +130,4 @@ for path4 in os.listdir(Revenue_Data):
         wb2.Close(True)
 
 #excel.Application.Quit()
+# TODO : Save As the excel file 
