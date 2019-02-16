8b852b3e8dc5d076d3e0471143f14337bc4e94bc
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-02-16 16:40:06 +0800

    (3rd Update) 21DaysReportAutomation
    - Change the Room Block Location from ID to Abbre
    - Modify the soql format

--- Salesforce/21DaysReportAutomation/21DaysReportAutomation.py
+++ Salesforce/21DaysReportAutomation/21DaysReportAutomation.py
@@ -3,7 +3,7 @@
 # with distribution list.
 
 from simple_salesforce import Salesforce, SalesforceLogin
-import requests, password, datetime, openpyxl
+import requests, password, datetime
 import stripJunkSimpleSalesforce as stripForce
 import pandas as pd
 
@@ -27,23 +27,27 @@ TabName = ['PROS', 'TENT']
 # Run for both Prospect and Tentative status for Booking tab
 for s, n in zip(Status, TabName) :
     # Booking tab - Use SOQL languauges to export the Booking tab from Salesforce
-    BKdata1 = sf.query("SELECT Owner.Name, Owner.Email, nihrm__Account__r.name, nihrm__Agency__r.name, nihrm__Property__c, Name, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, nihrm__BookingTypeName__c, nihrm__ForecastRoomnightsTotal__c, nihrm__DecisionDate__c, nihrm__BookedDate__c FROM nihrm__Booking__c WHERE (nihrm__BookingStatus__c = '" + str(s) + "') AND (nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert')) AND (nihrm__ArrivalDate__c >= TODAY AND nihrm__ArrivalDate__c <= " + str(End_Date) + ") AND (nihrm__Property__c NOT IN ('Sands Macao Hotel'))")
+    BKdata1 = sf.query("SELECT Owner.Name, Owner.Email, nihrm__Account__r.name, nihrm__Agency__r.name, nihrm__Property__c, Name, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, nihrm__BookingTypeName__c, nihrm__ForecastRoomnightsTotal__c, nihrm__DecisionDate__c, nihrm__BookedDate__c \
+                       FROM nihrm__Booking__c \
+                       WHERE (nihrm__BookingStatus__c = '" + str(s) + "') AND (nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert')) AND (nihrm__ArrivalDate__c >= TODAY AND nihrm__ArrivalDate__c <= " + str(End_Date) + ") AND (nihrm__Property__c NOT IN ('Sands Macao Hotel'))")
     # Convert the data to a readable format
     BKdata2 = stripForce.stripJunkSimpleSalesforce(BKdata1)
     # Sorting the order for the columns
-    index = ['Owner.Name', 'nihrm__Property__c', 'nihrm__Account__r.Name', 'nihrm__Agency__r.Name', 'Name', 'nihrm__ArrivalDate__c', 'nihrm__DepartureDate__c', 'nihrm__BookingTypeName__c', 'nihrm__ForecastRoomnightsTotal__c', 'nihrm__DecisionDate__c', 'nihrm__BookedDate__c', 'Owner.Email']
+    index = ['Owner.Name', 'nihrm__Property__c', 'nihrm__Account__r.Name', 'nihrm__Agency__r.Name', 'Name', 'nihrm__ArrivalDate__c', 'nihrm__DepartureDate__c', \
+             'nihrm__BookingTypeName__c', 'nihrm__ForecastRoomnightsTotal__c', 'nihrm__DecisionDate__c', 'nihrm__BookedDate__c', 'Owner.Email']
     BKdata3 = pd.DataFrame.from_dict(BKdata2)
     BKdata4 = pd.DataFrame(BKdata3, columns = index)
     
-    
-    # Property (Location) Code to exclude in the report -- need to ask Amademus
-    ExcludeProp = "('a0Y28000001RJ07', 'a0Y28000001RJ0A', 'a0Y28000001RJ0B', 'a0Y28000001RJ0C')"
+    # Property (Location) Code to exclude in the report
+    ExcludeProp = "('FSHM', 'SGMH', 'SANDS', 'TSRM')"
     # Room Block tab - Use SOQL languauges to export the Room Block tab from Salesforce
-    RBdata1 = sf.query("SELECT Owner.Name, nihrm__Location__c, nihrm__StartDate__c, Name, nihrm__Booking__r.nihrm__BookingTypeName__c, nihrm__RoomBlockStatus__c, nihrm__Booking__r.Name, nihrm__ForecastRoomnightsTotal__c FROM nihrm__BookingRoomBlock__c WHERE nihrm__Booking__r.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert') AND (nihrm__RoomBlockStatus__c = '" + str(s) + "') AND (nihrm__StartDate__c >= TODAY AND nihrm__StartDate__c <= " + str(End_Date) + ") AND (nihrm__Location__c NOT IN " + str(ExcludeProp) + ")")
+    RBdata1 = sf.query("SELECT Owner.Name, nihrm__Location__r.Name, nihrm__StartDate__c, Name, nihrm__Booking__r.nihrm__BookingTypeName__c, nihrm__RoomBlockStatus__c, nihrm__Booking__r.Name, nihrm__ForecastRoomnightsTotal__c \
+                       FROM nihrm__BookingRoomBlock__c \
+                       WHERE nihrm__Booking__r.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert') AND (nihrm__RoomBlockStatus__c = '" + str(s) + "') AND (nihrm__StartDate__c >= TODAY AND nihrm__StartDate__c <= " + str(End_Date) + ") AND (nihrm__Location__r.Name NOT IN " + str(ExcludeProp) + ")")
     # Convert the data to a readable format
     RBdata2 = stripForce.stripJunkSimpleSalesforce(RBdata1)
     # Sorting the order for the columns
-    index = ['nihrm__Location__c', 'nihrm__Booking__r.Name', 'Name', 'Owner.Name', 'nihrm__Booking__r.nihrm__BookingTypeName__c', 'nihrm__RoomBlockStatus__c', 'nihrm__StartDate__c', 'nihrm__ForecastRoomnightsTotal__c']
+    index = ['nihrm__Location__r.Name', 'nihrm__Booking__r.Name', 'Name', 'Owner.Name', 'nihrm__Booking__r.nihrm__BookingTypeName__c', 'nihrm__RoomBlockStatus__c', 'nihrm__StartDate__c', 'nihrm__ForecastRoomnightsTotal__c']
     RBdata3 = pd.DataFrame.from_dict(RBdata2)
     RBdata4 = pd.DataFrame(RBdata3, columns = index)
     
