f6bfa180bfe5bbf258390a93105118704b7ec61c
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-03-15 16:22:31 +0800

    (2nd Update) WholesaleMonthlyReportAutomation
    - Create Wholesale Data excel for copying
    - Paste value to get rid of formula
    - Stop Auto Run for Revenue excel file
    - Copy value from Revenue excel to our Wholesale Date excel

--- Working/WholesalesMonthlyReportAutomation/WholesaleMonthlyReportAutomation.py
+++ Working/WholesalesMonthlyReportAutomation/WholesaleMonthlyReportAutomation.py
@@ -6,7 +6,8 @@ import os.path, shutil, PathNPassword
 from pathlib import Path
 import win32com.client as win32
 from win32com.client import constants
-excel = win32.gencache.EnsureDispatch('Excel.Application')
+excel = win32.DispatchEx("Excel.Application")
+from ctypes import windll
 
 
 # Import the password and working file path from PathNPassword
@@ -17,28 +18,65 @@ password = PathNPassword.password
 # Create an empty Wholesale Date excel file
 Wholesale_Data_Path = PathNPassword.Wholesale_Data_Path
 Wholesale_Data = PathNPassword.Wholesale_Data
+
+# Properties in Working File
+Properties = ['VMRH', 'CMCC', 'HICC', 'PARIS']
+
+# Create Worksheet for each Properties
 wb_WSData = excel.Workbooks.Add()
-wb_WSData.SaveAs(Wholesale_Data_Path + Wholesale_Data)
+excel.Calculation = -4135
+excel.Visible = True
+for prop in Properties:
+    wsP = wb_WSData.Worksheets.Add()
+    wsP.Name = prop
+# Delete empty worksheets
+for sheet in wb_WSData.Worksheets:
+    if "Sheet" in sheet.Name:
+        sheet.Delete()
+
+#wb_WSData.SaveAs(Wholesale_Data_Path + Wholesale_Data)
 
 # Import the Revenue Path according to month
 Revenue_Path = PathNPassword.Revenue_Path
 
-#for filename in os.listdir(Revenue_Path):
-#    if filename.endswith('.xlsx') or filename.endswith('.xlsm'):
-        
+def CopyNPaste(filename, prop):
+    wb1 = excel.Workbooks.Open(filename, False, False, None)
+    ws1 = wb1.Worksheets('All_MTD')
+    RowRange = ws1.Range("B1:B400")
+    for i, value in enumerate(RowRange, 1):
+        if "grand total" in str(value).lower():
+            LastCellNum = i
+            break
 
-# TODO: Open each of the excel file for all properties
-    # TODO: Turn off the Calculate Now
-    # TODO: Copy and paste the value
-    # TODO: Change the tab name according to the property
-wb_WSData.Close(True)
+    CopyRange = ws1.Range(ws1.Cells(1,1), ws1.Cells(LastCellNum, 26))
+    CopyRange.Copy()
+    ws2 = wb_WSData.Worksheets(prop)
+    ws2.Range(ws2.Cells(1,1), ws2.Cells(LastCellNum, 26)).PasteSpecial(Paste = constants.xlPasteValues, Operation = constants.xlNone)
+    # Clear Clipboard (for warning window appear while running)
+    if windll.user32.OpenClipboard(None):
+        windll.user32.EmptyClipboard()
+        windll.user32.CloseClipboard()
+    wb1.Close(True)
+
+for filename in os.listdir(Revenue_Path):
+    if "Venetian" in filename:
+        CopyNPaste(Revenue_Path + filename, 'VMRH')
+    elif "Conrad" in filename:
+        CopyNPaste(Revenue_Path + filename, 'CMCC')
+    elif "Holiday" in filename:
+        CopyNPaste(Revenue_Path + filename, 'HICC')
+    elif "Parisian" in filename:
+        CopyNPaste(Revenue_Path + filename, 'PARIS')
+"""
+#wb_WSData.Close(True)
 
 # Open working file with password
-wb1 = excel.Workbooks.Open(Working_File, False, False, None, password, password)
-excel.Visible = True
+#wb2 = excel.Workbooks.Open(Working_File, False, False, None, password, password)
+#excel.Visible = True
 
 # TODO: Save as a new file
 
 # TODO: Click the macro button
 
-excel.Application.Quit()
+#excel.Application.Quit()
+"""
