8f6c17905b40be1ba13dc7cb33a68826ac51185d
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-08-28 17:50:02 +0800

    (2nd Update) HotelSalesReportAutomation.py
    - Modify the SOQL query
    - Rename column and re-arrange the column order

--- Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
+++ Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
@@ -1,5 +1,5 @@
 #! python3
-# HotelSalesReport.py - run data from salesforce, get data and export to excel. Send the excel
+# HotelSalesReport.py - run data from salesforce, get data and export to excel
 
 
 from simple_salesforce import Salesforce
@@ -20,9 +20,8 @@ proxies = {"http": "http://10.96.250.10:80", "https":"https://10.96.250.10:80"}
 
 # Date Range for Current Year
 now = datetime.datetime.now()
-EndDate = now + datetime.timedelta(days=21)
 Start_Date = str(now.year) + '-' + str('01') + '-' + str('01')
-End_Date = str(EndDate.year) + '-' + str('12') + '-' + str('31')
+End_Date = str(now.year) + '-' + str('12') + '-' + str('31')
 FileDate = str(now.year) + str('%02d'% now.month) + str('%02d'% now.day)
 
 # Login to Salesforce
@@ -30,14 +29,20 @@ sf = Salesforce(instance='na1.salesforce.com', session_id='', proxies=proxies, u
 session = requests.Session()
 
 # Run SOQL to get Hotel Sales Report data
-BKdata1 = sf.query("SELECT nihrm__Property__c, End_User_Region__c, nihrm__BookingTypeName__c, RSO_Manager__r.Name, Owner.Name, nihrm__Account__r.Name, Name, nihrm__BookedDate__c, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, \
-                           nihrm__CurrentBlendedRoomnightsTotal__c, 	nihrm__BlendedGuestroomRevenueTotal__c, VCL_Blended_F_B_Revenue__c, 	nihrm__CurrentBlendedEventRevenue7__c, nihrm__CurrentBlendedEventRevenue3__c, nihrm__LastStatusDate__c, nihrm__BookingStatus__c, nihrm__StatusReasonName__c\
+BKdata1 = sf.query("SELECT nihrm__Property__c, End_User_Region__c, nihrm__BookingTypeName__c, VCL_Booking Team_N, Owner.Name, RSO_Manager__r.Name, nihrm__Account__r.Name, Name, nihrm__BookedDate__c, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, \
+                           nihrm__CurrentBlendedRoomnightsTotal__c, 	nihrm__BlendedGuestroomRevenueTotal__c, nihrm__CurrentBlendedADR__c, VCL_Blended_F_B_Revenue__c, 	nihrm__CurrentBlendedEventRevenue7__c, nihrm__CurrentBlendedEventRevenue3__c, nihrm__LastStatusDate__c, nihrm__BookingStatus__c\
                     FROM nihrm__Booking__c \
                     WHERE (NOT nihrm__BookingTypeName__c IN ('ALT Alternative', 'Default', 'IN Internal', 'CN Concert')) AND (nihrm__BookingStatus__c IN ('Definite', 'Tentative', 'Prospect', 'Cancelled', 'TurnedDown')) AND (NOT nihrm__Property__c IN ('Sands Macao Hotel')) AND \
                     (NOT nihrm__StatusReasonName__c IN ('Alternative booking', 'Operator Error in Entry')) AND (nihrm__BookedDate__c >= " + str(Start_Date) + " AND nihrm__BookedDate__c <= " + str(End_Date) + ")")
 
 # Convert the data to a readable format
 BKdata2 = stripForce.stripJunkSimpleSalesforce(BKdata1)
+#print(BKdata2)
+index = ['nihrm__Property__c', 'End_User_Region__c', 'nihrm__BookingTypeName__c', 'VCL_Booking Team_N', 'Owner.Name', 'RSO_Manager__r', 'nihrm__Account__r.Name', 'Name', 'nihrm__BookedDate__c', 'nihrm__ArrivalDate__c', 'nihrm__DepartureDate__c',
+         'nihrm__CurrentBlendedRoomnightsTotal__c', 'nihrm__BlendedGuestroomRevenueTotal__c', 'nihrm__CurrentBlendedADR__c', 'VCL_Blended_F_B_Revenue__c', 'nihrm__CurrentBlendedEventRevenue7__c', 'nihrm__CurrentBlendedEventRevenue3__c', 'nihrm__LastStatusDate__c', 'nihrm__BookingStatus__c']
+BKdata3 = pd.DataFrame(pd.DataFrame.from_dict(BKdata2), columns = index)
+BKdata3.columns = ['Property', 'End User Region', 'BkgType', 'Group', 'BkdByID', 'BookedBy', 'RSO', 'Account Name', 'PostAs', 'CreateDate', 'ArrivalDate', 'BookedDate', 'DepartureDate', 'Room Night', ' Room Night Rev', 'AverageRate',
+                   'F&B Revenue', 'Venue', 'Others', 'StatusChangeDate', 'Status']
+#BKdata3[]
+print(BKdata3)
 
-BKdata3 = pd.DataFrame(pd.DataFrame.from_dict(BKdata2))
-print(BKdata3.head())
