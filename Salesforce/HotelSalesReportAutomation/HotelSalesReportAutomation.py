1589dad10dcdfe556fdc6c0ea4a712a98bf2c308
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-08-30 12:07:33 +0800

    (5th Update) HotelSalesReportAutomation.py
    - Modify Dictionary for Date value
    - Debug

--- Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
+++ Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
@@ -39,23 +39,26 @@ BKdata1 = sf.query("SELECT nihrm__Property__c, End_User_Region__c, nihrm__Bookin
 BKdata2 = stripForce.stripJunkSimpleSalesforce(BKdata1)
 BKdata3 = pd.DataFrame.from_dict(BKdata2)
 
+# Rename column name
+BKdata3.rename(columns={'nihrm__BookedDate__c': 'CreateDate', 'nihrm__ArrivalDate__c': 'ArrivalDate', 
+                        'nihrm__DepartureDate__c': 'DepartureDate', 'nihrm__LastStatusDate__c': 'StatusChangeDate'}, inplace=True)
 # Date columns in List for process
-DateFormat = ['nihrm__BookedDate__c', 'nihrm__ArrivalDate__c', 'nihrm__DepartureDate__c', 'nihrm__LastStatusDate__c']
+DateFormat = ['CreateDate', 'ArrivalDate', 'DepartureDate', 'StatusChangeDate']
 # Convert column format to datetime
 for date in DateFormat:
     BKdata3[str(date)] = pd.to_datetime(BKdata3[str(date)])
 
 # Create columns to fit Excel report
 # Add 'BookedDate' column - same date as CreatedDate
-BKdata3['BookedDate'] = BKdata3['nihrm__BookedDate__c']
+BKdata3['BookedDate'] = BKdata3['CreateDate']
 # Add 'Group' column - Show SM for Sales Manager or RSM for RSO
 BKdata3['Group'] = BKdata3['RSO_Manager__r.Name'].apply(lambda x: 'SM' if pd.isnull(x) else 'RSM')
 # Add 'Period' column - Display the Year and month
-BKdata3['Period'] = pd.to_datetime(BKdata3['nihrm__BookedDate__c']).dt.strftime('%Y-%m')
+BKdata3['Period'] = pd.to_datetime(BKdata3['CreateDate']).dt.strftime('%Y-%m')
 # Add 'Total' column - Use (Room Night * AverageRate) to calculate the revenue
 BKdata3['Total'] = BKdata3['nihrm__CurrentBlendedRoomnightsTotal__c'] * BKdata3['nihrm__CurrentBlendedADR__c']
 # Add 'Lead Time' column - Add and calculate the difference between Arrival to Booked Date
-BKdata3['Lead Time'] = BKdata3['nihrm__ArrivalDate__c'] - BKdata3['nihrm__BookedDate__c']
+BKdata3['Lead Time'] = BKdata3['ArrivalDate'] - BKdata3['CreateDate']
 BKdata3['Lead Time'] = (BKdata3['Lead Time'] / np.timedelta64(1, 'D')) + 1
 # Add 'Count_Status' column - Display '1' if Status in Definite, and '0' for other status
 BKdata3['Count_Status'] = BKdata3['nihrm__BookingStatus__c'].apply(lambda x: 1 if x == 'Definite' else 0)
@@ -65,8 +68,8 @@ BKdata3['Lead_Status'] = BKdata3['Count_Status']
 BKdata3['Lead_all'] = BKdata3['nihrm__BookingStatus__c'].apply(lambda x: 0 if x == '' else 1)
 
 # Sort Column Order
-index = ['nihrm__Property__c', 'End_User_Region__c', 'nihrm__BookingTypeName__c', 'Group', 'VCL_Booking_Team_N__c', 'Owner.Name', 'RSO_Manager__r.Name', 'nihrm__Account__r.Name', 'Name', 'nihrm__BookedDate__c', 'nihrm__ArrivalDate__c', 'BookedDate', 'nihrm__DepartureDate__c', 'nihrm__CurrentBlendedRoomnightsTotal__c', 'nihrm__BlendedGuestroomRevenueTotal__c', 
-         'nihrm__CurrentBlendedADR__c', 'VCL_Blended_F_B_Revenue__c', 'nihrm__CurrentBlendedEventRevenue7__c', 'nihrm__CurrentBlendedEventRevenue3__c', 'nihrm__LastStatusDate__c', 'nihrm__BookingStatus__c', 'nihrm__StatusReasonName__c', 'Period', 'Total', 'Lead Time', 'Count_Status', 'Lead_Status', 'Lead_all']
+index = ['nihrm__Property__c', 'End_User_Region__c', 'nihrm__BookingTypeName__c', 'Group', 'VCL_Booking_Team_N__c', 'Owner.Name', 'RSO_Manager__r.Name', 'nihrm__Account__r.Name', 'Name', 'CreateDate', 'ArrivalDate', 'BookedDate', 'DepartureDate', 'nihrm__CurrentBlendedRoomnightsTotal__c', 'nihrm__BlendedGuestroomRevenueTotal__c', 
+         'nihrm__CurrentBlendedADR__c', 'VCL_Blended_F_B_Revenue__c', 'nihrm__CurrentBlendedEventRevenue7__c', 'nihrm__CurrentBlendedEventRevenue3__c', 'StatusChangeDate', 'nihrm__BookingStatus__c', 'nihrm__StatusReasonName__c', 'Period', 'Total', 'Lead Time', 'Count_Status', 'Lead_Status', 'Lead_all']
 BKdata3 = pd.DataFrame(BKdata3, columns = index)
 # Drop 'nihrm__StatusReasonName__c' column
 BKdata3.drop('nihrm__StatusReasonName__c', axis=1, inplace=True)
@@ -81,6 +84,6 @@ BKdata3.columns = ['Property', 'End User Region', 'BkgType', 'Group', 'BkdByID',
 for date in DateFormat:
     BKdata3[str(date)] = BKdata3[str(date)].dt.strftime('%d/%m/%Y')
     
-print(BKdata3.head())
+print(BKdata3[['CreateDate', 'ArrivalDate', 'DepartureDate', 'StatusChangeDate', 'Status']].head(10))
 
 # TODO: Calculate top 3 Region for 'No of Definite', 'Definite RNs', 'Total Leads', 'Total Deman'
