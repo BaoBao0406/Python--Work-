a83179e46f73463e7598c6101baf38e37ef1cfbb
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2019-09-02 17:16:43 +0800

    (7th Update) HotelSalesReportAutomation.py
    - Change Pivot Table to Groupby for Top3 Region
    - SOQL query for Sheraton room block
    - Rename for Sheraton room block

--- Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
+++ Salesforce/HotelSalesReportAutomation/HotelSalesReportAutomation.py
@@ -29,7 +29,7 @@ sf = Salesforce(instance='na1.salesforce.com', session_id='', proxies=proxies, u
 session = requests.Session()
 
 # Run SOQL to get Hotel Sales Report data
-BKdata1 = sf.query("SELECT nihrm__Property__c, End_User_Region__c, nihrm__BookingTypeName__c, VCL_Booking_Team_N__c, Owner.Name, RSO_Manager__r.Name, nihrm__Account__r.Name, Name, nihrm__BookedDate__c, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, \
+BKdata1 = sf.query("SELECT nihrm__Property__c, End_User_Region__c, nihrm__BookingTypeName__c, VCL_Booking_Team_M__c, Owner.Name, RSO_Manager__r.Name, nihrm__Account__r.Name, Name, nihrm__BookedDate__c, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, \
                            nihrm__CurrentBlendedRoomnightsTotal__c, 	nihrm__BlendedGuestroomRevenueTotal__c, nihrm__CurrentBlendedADR__c, VCL_Blended_F_B_Revenue__c, nihrm__CurrentBlendedEventRevenue7__c, nihrm__CurrentBlendedEventRevenue3__c, nihrm__LastStatusDate__c, nihrm__BookingStatus__c, nihrm__StatusReasonName__c \
                     FROM nihrm__Booking__c \
                     WHERE (NOT nihrm__BookingTypeName__c IN ('ALT Alternative', 'Default', 'IN Internal', 'CN Concert')) AND (nihrm__BookingStatus__c IN ('Definite', 'Tentative', 'Prospect', 'Cancelled', 'TurnedDown')) AND (NOT nihrm__Property__c IN ('Sands Macao Hotel')) AND \
@@ -70,7 +70,7 @@ BKdata3['Lead_all'] = BKdata3['nihrm__BookingStatus__c'].apply(lambda x: 0 if x 
 BKdata3['StatusChangeDate'] = BKdata3['StatusChangeDate'].apply(lambda x: None if x=="NaT" else x)
 
 # Sort Column Order
-index = ['nihrm__Property__c', 'End_User_Region__c', 'nihrm__BookingTypeName__c', 'Group', 'VCL_Booking_Team_N__c', 'Owner.Name', 'RSO_Manager__r.Name', 'nihrm__Account__r.Name', 'Name', 'CreateDate', 'ArrivalDate', 'BookedDate', 'DepartureDate', 'nihrm__CurrentBlendedRoomnightsTotal__c', 'nihrm__BlendedGuestroomRevenueTotal__c', 
+index = ['nihrm__Property__c', 'End_User_Region__c', 'nihrm__BookingTypeName__c', 'Group', 'VCL_Booking_Team_M__c', 'Owner.Name', 'RSO_Manager__r.Name', 'nihrm__Account__r.Name', 'Name', 'CreateDate', 'ArrivalDate', 'BookedDate', 'DepartureDate', 'nihrm__CurrentBlendedRoomnightsTotal__c', 'nihrm__BlendedGuestroomRevenueTotal__c', 
          'nihrm__CurrentBlendedADR__c', 'VCL_Blended_F_B_Revenue__c', 'nihrm__CurrentBlendedEventRevenue7__c', 'nihrm__CurrentBlendedEventRevenue3__c', 'StatusChangeDate', 'nihrm__BookingStatus__c', 'nihrm__StatusReasonName__c', 'Period', 'Total', 'Lead Time', 'Count_Status', 'Lead_Status', 'Lead_all']
 BKdata3 = pd.DataFrame(BKdata3, columns = index)
 # Drop 'nihrm__StatusReasonName__c' column
@@ -80,25 +80,42 @@ BKdata3.drop('nihrm__StatusReasonName__c', axis=1, inplace=True)
 BKdata3.columns = ['Property', 'End User Region', 'BkgType', 'Group', 'BkdByID', 'BookedBy', 'RSO', 'Account Name', 'PostAs', 'CreateDate', 'ArrivalDate', 'BookedDate', 'DepartureDate', 'Room Night', ' Room Night Rev', 'AverageRate',
                    'F&B Revenue', 'Venue', 'Others', 'StatusChangeDate', 'Status', 'Period', 'Total', 'Lead Time', 'Count_Status', 'Lead_Status', 'Lead_all']
 
-# TODO: take out Sheraton RN and Revenue from the data
-
 # Convert date column format to suit excel format
 for date in DateFormat:
     BKdata3[str(date)] = BKdata3[str(date)].dt.strftime('%d/%m/%Y')
 
-# Create Pivot Table for 'No of Definite', 'Total Leads', 'Total Demand'
-Region1Top3 = pd.pivot_table(BKdata3, index='End User Region', values = ['Count_Status', 'Room Night', 'Lead_all'], aggfunc = 'sum')
-# Calculate top 3 Region for 'No of Definite'
-No_of_Def = Region1Top3.sort_values(by='Count_Status', ascending=False).head(3)
-print(No_of_Def)
-# Calculate top 3 Region for 'Total Leads'
-Total_Leads = Region1Top3.sort_values(by='Lead_all', ascending=False).head(3)
-print(Total_Leads)
-# Calculate top 3 Region for 'Total Demand'
-Total_Demand = Region1Top3.sort_values(by='Room Night', ascending=False).head(3)
-print(Total_Demand)
+"""
+# TODO: take out Sheraton RN and Revenue from the data
+SheratonData1 =  = sf.query("SELECT nihrm__Location__r.Name, nihrm__Booking__r.Name, nihrm__ForecastRoomnightsTotal__c, nihrm__ForecastRevenueTotal__c, nihrm__BookedDate__c, Name, nihrm__Booking__r.nihrm__BookingTypeName__c, nihrm__RoomBlockStatus__c \
+                             FROM nihrm__BookingRoomBlock__c \
+                             WHERE nihrm__Booking__r.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert', 'ALT Alternative') AND (nihrm__RoomBlockStatus__c = '" + str(s) + "') AND (nihrm__BookedDate__c >= " + str(Start_Date) + "  AND nihrm__BookedDate__c <= " + str(End_Date) + ") AND (nihrm__Location__r.Name IN ('FSHM', 'SGMH', 'TSRM'))")
 
-# Create Pivot Table for 'Definite RNs'
-Region2Top3 = pd.pivot_table(BKdata3.loc[BKdata3['Count_Status']>0], index='End User Region', values = 'Room Night', aggfunc= 'sum')
+# Convert the data to a readable format
+SheratonData2 = stripForce.stripJunkSimpleSalesforce(SheratonData1)
+SheratonData3 = pd.DataFrame.from_dict(SheratonData2)
+# Rename Sheraton column name
+SheratonData3.columns = ['Property', 'Post As', 'Room Night', ' Room Night Rev', 'CreateDate', 'RoomBlockName','BookingType', 'Status']
+
+# Sumif function for Roomnights in Room Block tab into Booking tab
+x = 0
+RN = RBdata3.groupby(['Post As'])['Roomnights'].sum()
+while RN.count()-1 >= x:
+    for i, j in BKdata3.iterrows():    
+        if RN.index[x] == j['Post As']:
+            BKdata3.at[i, 'Roomnights'] = RN.values[x]
+    x += 1
+"""
+# All lead Top3 - Create Groupby for Top3 data
+Region1Top3 = BKdata3[['End User Region', 'Room Night', 'Lead_all']].groupby('End User Region').sum()
+# Total_Demand - Top 3 Region for 'Total Demand'
+Total_Demand = Region1Top3.sort_values('Room Night', ascending=False).head(3)
+# Total_Leads - Top 3 Region for 'Total Leads'
+Total_Leads = Region1Top3.sort_values('Lead_all', ascending=False).head(3)
+
+# Def lead Top3 - Create Groupby for Top3 Definite data
+Region2Top3 = BKdata3[BKdata3['Count_Status'] == 1]
+Region2Top3 = Region2Top3[['End User Region', 'Room Night', 'Count_Status']].groupby('End User Region').sum()
+# Def_RN - Top 3 Region for 'Definite RNs'
 Def_RN = Region2Top3.sort_values(by='Room Night', ascending=False).head(3)
-print(Def_RN)
+# Def_Leads - Top 3 Region for 'No of Definite'
+Def_Leads = Region2Top3.sort_values(by='Count_Status', ascending=False).head(3)
