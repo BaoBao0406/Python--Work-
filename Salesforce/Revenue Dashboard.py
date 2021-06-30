76efdff26be3799b77d62164e3129676b77e34a9
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2021-06-30 17:34:25 +0800

    1st Update
    update Query "01Rawdata_Group Booking by arrival date"
    finish function for convert to excel

--- Salesforce/Revenue Dashboard.py
+++ Salesforce/Revenue Dashboard.py
@@ -14,15 +14,32 @@ EndDate = now + datetime.timedelta(days=21)
 Current_Date = str(now.year) + '-' + str('%02d'% now.month) + '-' + str('%02d'% now.day)
 End_Date = str(EndDate.year) + '-' + str('%02d'% EndDate.month) + '-' + str('%02d'% EndDate.day)
 
-# TODO: Convert data to excel format
+
+# Convert data to excel format
 def convert_to_excel(data, filename):
-    
-    return 
+    data.to_excel(save_path + filename + '.xlsx', sheet_name='Sheet1')
 
 
-# TODO: "01Rawdata_Group Booking by arrival date" - Report
-data = pd.read_sql('SELECT * \
-                    FROM dbo.nihrm__Booking__c', conn)
+# "01Rawdata_Group Booking by arrival date" - Report
+data = pd.read_sql("SELECT BK.OwnerId, BK.nihrm__Property__c, ac.Name, ac.BillingCity, ac.BillingCountry, ag.Name, BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'dd/MM/yyyy') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'dd/MM/yyyy') AS DepartureDate, \
+                           BK.Percentage_of_Attrition__c, BK.nihrm__CommissionPercentage__c, BK.Promotion__c, BK.nihrm__AtDefiniteAgreedRoomnights__c, BK.nihrm__CurrentBlendedRoomnightsTotal__c, BK.nihrm__AtDefiniteAgreedGuestroomRevenue__c, BK.nihrm__BlendedGuestroomRevenueTotal__c, \
+                           BK.nihrm__AtDefiniteBlendedEventRevenue1__c, BK.nihrm__CurrentBlendedEventRevenue1__c, BK.nihrm__AtDefiniteBlendedEventRevenue2__c, BK.nihrm__CurrentBlendedEventRevenue2__c, BK.nihrm__AtDefiniteBlendedEventRevenue9__c, BK.nihrm__CurrentBlendedEventRevenue9__c, \
+                           BK.VCL_Blended_F_B_Revenue__c, BK.nihrm__AtDefiniteBlendedEventRevenue7__c, BK.nihrm__CurrentBlendedEventRevenue7__c, BK.nihrm__AtDefiniteBlendedEventRevenue4__c, BK.nihrm__CurrentBlendedEventRevenue4__c, \
+                           BK.nihrm__AtDefiniteBlendedEventRevenue3__c, BK.nihrm__CurrentBlendedEventRevenue3__c, BK.nihrm__AtDefiniteBlendedEventRevenue8__c, BK.nihrm__CurrentBlendedEventRevenue8__c, BK.nihrm__AtDefiniteBlendedEventRevenue6__c, \
+                           BK.nihrm__CurrentBlendedEventRevenue6__c, BK.Sheraton_F_B_Revenue__c, BK.Sheraton_Room_Rental_Revenue__c, BK.nihrm__BookingStatus__c, FORMAT(BK.nihrm__LastStatusDate__c, 'dd/MM/yyyy') AS LastStatusDate, \
+                           FORMAT(BK.nihrm__BookedDate__c, 'dd/MM/yyyy') AS BookedDate, BK.End_User_Region__c, BK.End_User_SIC__c, BK.nihrm__BookingTypeName__c, BK.nihrm__LostToCompetitorName__c, BK.nihrm__StatusReasonName__c, BK.Booking_ID_Number__c \
+                    FROM dbo.nihrm__Booking__c AS BK \
+                    LEFT JOIN dbo.Account AS ac \
+                        ON BK.nihrm__Account__c = ac.Id \
+                    LEFT JOIN dbo.Account AS ag \
+                        ON BK.nihrm__Agency__c = ag.Id", conn)
+data.columns = ['Booking: Owner Name', 'Property', 'Account', 'Company City', 'Company Country', 'Agency', 'Booking: Booking Post As', 'Arrival', 'Departure', 'Percentage of Attrition', 'Commission %', 'Promotion', 'At Definite Agreed Roomnights',
+                'Blended Roomnights', 'At Definite Agreed Guestroom Revenue', 'Blended Guestroom Revenue Total', 'At Definite Blended Food Revenue', 'Blended Food Revenue', 'At Definite Blended Beverage Revenue', 'Blended Beverage Revenue',
+                'At Definite Blended Outlet Revenue', 'Blended Outlet Revenue', 'Blended F&B Revenue', 'At Definite Blended Rental Revenue', 'Blended Rental Revenue', 'At Definite Blended AV Revenue', 'Blended AV Revenue', 'At Definite Blended Resource Revenue',
+                'Blended Resource Revenue', 'At Definite Blended Ancillary Revenue', 'Blended Ancillary Revenue', 'At Definite Blended Other Revenue', 'Blended Other Revenue', 'Sheraton F&B Revenue', 'Sheraton Room Rental Revenue	', 'Status',
+                'Last Status Date', 'Booked', 'End User Region', 'End User SIC', 'Booking Type', 'Lost to Competitor', 'Lost Reason', 'Booking ID#']
+filename = '01Rawdata_Group Booking by arrival date'
+convert_to_excel(data, filename)
 
 
 # TODO: "02Account and Activities" - Report
