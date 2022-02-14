ace06de6d38c65671e2b1b958a2a98ce100c50dd
BaoBao0406 <46430379+BaoBao0406@users.noreply.github.com>
2022-02-14 16:18:56 +0800

    3rd Update
    - change query method from SOQL to SQL 
    - amend the structure of the script

--- Salesforce/21DaysReportAutomation/21DaysReportAutomation.py
+++ Salesforce/21DaysReportAutomation/21DaysReportAutomation.py
@@ -1,55 +1,18 @@
 #! python3
-# 21DaysReportAutomation.py - run data from salesforce, get data and export to excel. Send the excel
+# 21DaysReportAutomation.py - run data from sql, get data and export to excel. Send the excel
 # with distribution list.
 
-from simple_salesforce import Salesforce
-import requests, datetime, os.path, password
-import stripJunkSimpleSalesforce as stripForce
+import datetime, os.path, mail_distribution_list, pyodbc
 import pandas as pd
 import win32com.client as win32
 from time import sleep
-import tkinter as tk
 excel = win32.gencache.EnsureDispatch('Excel.Application')
 outlook = win32.Dispatch("Outlook.Application")
 
-path = 'I:\\10-Sales\\01_Sales_Reports\\21 Days Report\\'
+##################################################################################################
 
-
-# Window for user to input password
-# function to get password
-def get_password():
-    global pw, us
-    us = user_entry.get()
-    pw = pw_entry.get()
-    window.destroy()
-
-# Create Window
-window = tk.Tk()
-window.title('Password')
-window.geometry("250x100+250+150")
-# Create entry space
-label = tk.Label(window, text='Enter password')
-label.pack()
-user_entry = tk.Entry(window, width=20)
-user_entry.pack()
-pw_entry = tk.Entry(window, width=20)
-pw_entry.pack()
-# Create button to excute function to get password from entry space
-button = tk.Button(window, text='Confirm', command=get_password)
-button.pack()
-window.mainloop()
-
-# Salesforce Login info
-Username = us
-Password = pw
-securitytoken = password.securitytoken
-
-# Change proxies and ports
-proxies = {"https":"https://10.96.250.11:443"}
-
-# Login to Salesforce
-sf = Salesforce(instance='na1.salesforce.com', session_id='', proxies=proxies, username = Username, password= Password, security_token = securitytoken)
-session = requests.Session()
+# filepath save
+path = 'I:\\10-Sales\\+Operational Reports (5Y, Restricted)\\2021\\21 Days Report\\'
 
 # Get Date for 21 days later
 now = datetime.datetime.now()
@@ -58,74 +21,115 @@ Current_Date = str(now.year) + '-' + str('%02d'% now.month) + '-' + str('%02d'% 
 End_Date = str(EndDate.year) + '-' + str('%02d'% EndDate.month) + '-' + str('%02d'% EndDate.day)
 FileDate = str(now.year) + str('%02d'% now.month) + str('%02d'% now.day)
 
-Status = ['Prospect', 'Tentative']
-TabName = ['PROS', 'TENT']
+# connect sql server
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=VOPPSCLDBN01\VOPPSCLDBI01;'
+                      'Database=SalesForce;'
+                      'Trusted_Connection=yes;')
+
+
+# FDC User ID, Name, Email list
+user = pd.read_sql('SELECT DISTINCT(Id), Name, Email \
+                    FROM dbo.[User]', conn)
+user_list = user.set_index('Id')['Name'].to_dict()
+user_email = user.set_index('Id')['Email'].to_dict()
+
+##################################################################################################
 
 # Adjust the column width (Credit To Stackoverflow: TrigonaMinima)
-def AdjustColumnWidth(Sheet, Name):
+def AdjustColumnWidth(writer, Sheet, Name):
     worksheet = writer.sheets[Sheet]
     for i, col in enumerate(Name.columns):
         column_len = Name[col].astype(str).str.len().max()
         column_len = max(column_len, len(col)) + 2
         worksheet.set_column(i, i, column_len)
 
-# Run for both Prospect and Tentative status for Booking tab
-for s, n in zip(Status, TabName):
-    # Booking tab
-    # Booking tab - Use SOQL languauges to export the Booking tab from Salesforce
-    BKdata1 = sf.query("SELECT Owner.Name, Owner.Email, nihrm__Account__r.name, nihrm__Agency__r.name, nihrm__Property__c, Name, nihrm__ArrivalDate__c, nihrm__DepartureDate__c, nihrm__BookingTypeName__c, nihrm__ForecastRoomnightsTotal__c, nihrm__DecisionDate__c, nihrm__BookedDate__c \
-                       FROM nihrm__Booking__c \
-                       WHERE (nihrm__BookingStatus__c = '" + str(s) + "') AND (nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert')) AND (nihrm__ArrivalDate__c >= TODAY AND nihrm__ArrivalDate__c <= " + str(End_Date) + ") AND (nihrm__Property__c NOT IN ('Sands Macao Hotel'))")
-    # Convert the data to a readable format
-    BKdata2 = stripForce.stripJunkSimpleSalesforce(BKdata1)
-    # Sorting the order for the columns
-    index = ['Owner.Name', 'nihrm__Property__c', 'nihrm__Account__r.Name', 'nihrm__Agency__r.Name', 'Name', 'nihrm__ArrivalDate__c', 'nihrm__DepartureDate__c', \
-              'nihrm__ForecastRoomnightsTotal__c', 'nihrm__DecisionDate__c', 'nihrm__BookedDate__c', 'nihrm__BookingTypeName__c', 'Owner.Email']
-    BKdata3 = pd.DataFrame(pd.DataFrame.from_dict(BKdata2), columns = index)
-    # Add to email distribution list
-    if n == "PROS":
-        ProsEmailList = list(set(BKdata3['Owner.Email'].tolist()))
-    elif n == "TENT":
-        TentEmailList = list(set(BKdata3['Owner.Email'].tolist()))
-    del BKdata3['Owner.Email']
-    # Change column header
-    BKdata3.columns = ['Booking Owner', 'Property', 'Account', 'Agency', 'Post As', 'Arrival', 'Departure', 'Roomnights', 'Decision Due', 'Booked Date', 'Booking Type']
-
-    # Room Block tab
-    # Property (Location) Code to exclude in the report
-    ExcludeProp = "('FSHM', 'SGMH', 'SANDS', 'TSRM')"
-    # Room Block tab - Use SOQL languauges to export the Room Block tab from Salesforce
-    RBdata1 = sf.query("SELECT Owner.Name, nihrm__Location__r.Name, nihrm__StartDate__c, Name, nihrm__Booking__r.nihrm__BookingTypeName__c, nihrm__RoomBlockStatus__c, nihrm__Booking__r.Name, nihrm__ForecastRoomnightsTotal__c \
-                       FROM nihrm__BookingRoomBlock__c \
-                       WHERE nihrm__Booking__r.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert') AND (nihrm__RoomBlockStatus__c = '" + str(s) + "') AND (nihrm__StartDate__c >= TODAY AND nihrm__StartDate__c <= " + str(End_Date) + ") AND (nihrm__Location__r.Name NOT IN " + str(ExcludeProp) + ")")
-    # Convert the data to a readable format
-    RBdata2 = stripForce.stripJunkSimpleSalesforce(RBdata1)
-    # Sorting the order for the columns
-    index = ['nihrm__Location__r.Name', 'nihrm__Booking__r.Name', 'Name', 'Owner.Name', 'nihrm__Booking__r.nihrm__BookingTypeName__c', 'nihrm__RoomBlockStatus__c', 'nihrm__StartDate__c', 'nihrm__ForecastRoomnightsTotal__c']
-    RBdata3 = pd.DataFrame(pd.DataFrame.from_dict(RBdata2), columns = index)
-    RBdata3.columns = ['Property', 'Post As', 'Room Block Name', 'Booking Owner', 'Booking Type', 'Status', 'Start Date', 'Roomnights']
-    
-    # Sumif function for Roomnights in Room Block tab into Booking tab
+# Sumif function for Roomnights in Room Block tab into Booking tab
+def sumif_function(BKdata, RBdata):
     x = 0
-    RN = RBdata3.groupby(['Post As'])['Roomnights'].sum()
+    RN = RBdata.groupby(['Post As'])['Roomnights'].sum()
     while RN.count()-1 >= x:
-        for i, j in BKdata3.iterrows():    
+        for i, j in BKdata.iterrows():    
             if RN.index[x] == j['Post As']:
-                BKdata3.at[i, 'Roomnights'] = RN.values[x]
+                BKdata.at[i, 'Roomnights'] = RN.values[x]
         x += 1
+    return BKdata
+
+# Booking SQL query
+def booking_sql_data(s):
+    BKdata = pd.read_sql("SELECT BK.OwnerId, BK.nihrm__Property__c, ac.Name, ag.Name,  BK.Name, FORMAT(BK.nihrm__ArrivalDate__c, 'MM/dd/yyyy') AS ArrivalDate, FORMAT(BK.nihrm__DepartureDate__c, 'MM/dd/yyyy') AS DepartureDate, \
+                                 BK.nihrm__ForecastRoomnightsTotal__c, FORMAT(BK.nihrm__DecisionDate__c, 'MM/dd/yyyy') AS LastStatusDate, FORMAT(BK.nihrm__BookedDate__c, 'MM/dd/yyyy') AS BookedDate, BK.nihrm__BookingTypeName__c \
+                          FROM dbo.nihrm__Booking__c AS BK \
+                          LEFT JOIN dbo.Account AS ac \
+                              ON BK.nihrm__Account__c = ac.Id \
+                          LEFT JOIN dbo.Account AS ag \
+                              ON BK.nihrm__Agency__c = ag.Id \
+                          WHERE (BK.nihrm__BookingStatus__c = '" + str(s) + "') AND (BK.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert')) AND \
+                                (BK.nihrm__ArrivalDate__c BETWEEN CONVERT(datetime, '" + Current_Date + "') AND CONVERT(datetime, '" + End_Date + "')) AND (BK.nihrm__Property__c NOT IN ('Sands Macao Hotel'))", conn)
+    BKdata.columns = ['Booking Owner', 'Property', 'Account', 'Agency', 'Post As', 'Arrival', 'Departure', 'Roomnights', 'Decision Due', 'Booked Date', 'Booking Type']
+    # create mail list
+    mail_list = BKdata[['Booking Owner']]
+    mail_list['Booking Owner'].replace(user_email, inplace=True)
+    # replace id with Owner Name
+    BKdata['Booking Owner'].replace(user_list, inplace=True)
+
+    return BKdata, mail_list
     
-    # Transfer the data to excel file
-    writer = pd.ExcelWriter(path + FileDate + '_21 days report ' + str(s) + '.xlsx', engine='xlsxwriter')
-    BKdata3.to_excel(writer, index=False, sheet_name=n)
-    # Adjust Column width
-    AdjustColumnWidth(n, BKdata3)          
+# Room Block SQL query
+def room_block_sql_data(s):
+    RBdata = pd.read_sql("SELECT prop.Name, BK.Name, RoomB.Name, RoomB.OwnerId, BK.nihrm__BookingTypeName__c, RoomB.nihrm__RoomBlockStatus__c, FORMAT(RoomB.nihrm__StartDate__c, 'MM/dd/yyyy') AS StartDate, \
+                                 RoomB.nihrm__ForecastRoomnightsTotal__c \
+                          FROM dbo.nihrm__BookingRoomBlock__c AS RoomB \
+                          LEFT JOIN dbo.nihrm__Booking__c AS BK \
+                              ON RoomB.nihrm__Booking__c = BK.Id \
+                          LEFT JOIN dbo.nihrm__Location__c AS prop \
+                              ON RoomB.nihrm__Location__c = prop.Id \
+                          WHERE (BK.nihrm__BookingTypeName__c NOT IN ('Default', 'IN Internal', 'CN Concert')) AND (RoomB.nihrm__RoomBlockStatus__c = '" + str(s) + "') AND \
+                                (prop.Name NOT IN ('Sheraton Grand Macao', 'Four Seasons Hotel Macao Cotai Strip', 'Sands Macao Hotel', 'The St. Regis Macao')) AND \
+                                (RoomB.nihrm__StartDate__c BETWEEN CONVERT(datetime, '" + Current_Date + "') AND CONVERT(datetime, '" + End_Date + "'))", conn)
+    RBdata.columns = ['Property', 'Post As', 'Room Block Name', 'Booking Owner', 'Booking Type', 'Status', 'Start Date', 'Roomnights']
+    RBdata['Booking Owner'].replace(user_list, inplace=True)
     
-    # Transfer the data to excel file
-    RBdata3.to_excel(writer, index=False, sheet_name="RN Block by Property")
-    # Adjust Column width
-    AdjustColumnWidth("RN Block by Property", RBdata3)
-    writer.save()
-    sleep(10)
+    return RBdata
+    
+
+# function to create P and T booking and room block data, then save as excel file
+def P_and_T_booking():
+    
+    Status = ['Prospect', 'Tentative']
+    TabName = ['PROS', 'TENT']
+    
+    for s, n in zip(Status, TabName):
+        # run Booking data
+        BKdata, mail_list = booking_sql_data(s)
+
+        # run Room Block data
+        RBdata = room_block_sql_data(s)
+        
+        # run sumif function
+        BKdata = sumif_function(BKdata, RBdata)
+        
+        # Transfer the data to excel file
+        writer = pd.ExcelWriter(path + FileDate + '_21 days report ' + str(s) + '.xlsx', engine='xlsxwriter')
+        BKdata.to_excel(writer, index=False, sheet_name=n)
+        # Adjust Column width
+        AdjustColumnWidth(writer, n, BKdata)          
+        
+        # Transfer the data to excel file
+        RBdata.to_excel(writer, index=False, sheet_name="RN Block by Property")
+        # Adjust Column width
+        AdjustColumnWidth(writer, "RN Block by Property", RBdata)
+        writer.save()
+        sleep(10)
+        
+        # create email distribution list for P and T
+        if n == "PROS":
+            ProsEmailList = list(set(mail_list['Booking Owner'].tolist()))
+        elif n == "TENT":
+            TentEmailList = list(set(mail_list['Booking Owner'].tolist()))
+        
+    return ProsEmailList, TentEmailList
+
 
 # Function to Create Prospect Email to send to DOS and SM
 def EmailPROS(ToList, CCList, SMList):
@@ -168,12 +172,18 @@ def EmailTENT(ToList, CCList, SMList):
     #mail.SaveAs(Path='I:\\10-Sales\\01_Sales_Reports\\21 Days Report\\Python Code\\Testing\\Tentative.msg')
     mail.send
 
-# Send two Emails to DOS and SM if Prospect report is larger than 0
-if len(ProsEmailList) > 0:
-    mail = outlook.CreateItem(0)
-    EmailPROS(password.ToListforPROS, password.CCListforPROS, ProsEmailList)
 
-# Send two Emails to DOS and SM if Tentative report is larger than 0
-if len(TentEmailList) > 0:
-    mail = outlook.CreateItem(0)
-    EmailTENT(password.ToListforTENT, password.CCListforTENT, TentEmailList)
+# main function
+if __name__ == '__main__':
+    # run 
+    ProsEmailList, TentEmailList = P_and_T_booking()
+
+    # Send two Emails to DOS and SM if Prospect report is larger than 0
+    if len(ProsEmailList) > 0:
+        mail = outlook.CreateItem(0)
+        EmailPROS(mail_distribution_list.ToListforPROS, mail_distribution_list.CCListforPROS, ProsEmailList)
+
+    # Send two Emails to DOS and SM if Tentative report is larger than 0
+    if len(TentEmailList) > 0:
+        mail = outlook.CreateItem(0)
+        EmailTENT(mail_distribution_list.ToListforTENT, mail_distribution_list.CCListforTENT, TentEmailList)
